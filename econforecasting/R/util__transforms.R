bf5c6e8c656b23cf151037558fb69fafc2997803
Charles Ye <charles@cmefi.com>
2021-02-17 19:09:09 -0500

    Added prep for Kalman smoothing model & additionally added expected inflations data and forecasting (v0.09)

--- econforecasting/R/util__transforms.R
+++ econforecasting/R/util__transforms.R
@@ -1,8 +1,9 @@
 #' Conducts common time series transformations - differencing, log-differencing, moving-averages, and lagged moving averages
 #'
 #' @param x (numeric vector) A vector of data to be transformed
-#' @param .l (integer) Number of laggs for taking a difference
+#' @param .l (integer) Number of lags for taking a difference
 #' @param .length (integer) Length of lags for taking moving average
+#' @param .h (integer vector) Non-transformed vector of historical values for reversing transformations
 #' @export
 
 
@@ -10,6 +11,10 @@ diff = function(x, .l) {
 	x - dplyr::lag(x, .l)
 }
 
+undiff = function(x, .l, .h) {
+    if (length(.h) < .l) stop('Length of .p must be greater than or equal to .l')
+    purrr::accumulate(x, function(accum, z) accum + z, .init = .h[length(.h) - .l + 1]) %>% .[2:length(.)]
+}
 
 diff1 = function(x) {
 	diff(x, .l = 1)
@@ -21,8 +26,14 @@ diff2 = function(x) {
 }
 
 
+
 dlog = function(x) {
 	log(x/dplyr::lag(x, 1))
+}
+
+undlog = function(x, .h) {
+    undiff(x = x, .l = 1, .h = log(.h)) %>%
+        exp(.)
 }
 
 
@@ -31,9 +42,14 @@ ma = function(x, .length) {
 		tibble(.) %>%
 		addLags(., .length - 1, .zero = TRUE) %>%
 		dplyr::rowwise(.) %>%
-		dplyr::mutate(mean = mean(.)) %>%
-		.$mean
+        dplyr::mutate(., m = rowMeans(across(where(is.numeric)))) %>%
+        .$m
 }
+
+ma2 = function(x) {
+    ma(x, 2)
+}
+
 
 lma = function(x, .length) {
 	x %>%
@@ -44,5 +60,17 @@ lma = function(x, .length) {
 		.$mean
 }
 
+lma2 = function(x) {
+    lma(x, 2)
+}
+
+
+apchg = function(x, .periods = 12) {
+    ((x/dplyr::lag(x, 1))^.periodsx - 1) * 100
+}
+
+unapchg = function(x, .periods = 12, .h) {
+    purrr::accumulate((x/100 + 1)^(1/.periods), function(accum, z) accum * z, .init = .h)
+}
 
 # tibble(thisEl = seq(1, 1.05, .01)) %>% purrr::transpose(.) %>% purrr::accumulate(., function(accum, x) list(thisEl = accum$thisEl + x$thisEl))
