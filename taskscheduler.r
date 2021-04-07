790a71b9d8ddda60c01e07139e530c75e9d5df83
Charles Ye <charles@cmefi.com>
2021-04-06 21:04:01 -0400

    Added task scheduler to automate scripts on Windows (v0.12)

---
+++ taskscheduler.r
@@ -0,0 +1,27 @@
+DIR = 'D:/OneDrive/__Projects/econforecasting'
+
+# Convert to R -> Run
+lapply(list('model-asset-contagion', 'model-baseline-forecasts', 'model-nowcasts'), function(filename) {
+	message('Started - ', filename)
+	if (file.exists(paste0(DIR, '/', filename, '.r'))) file.remove(paste0(DIR, '/', filename, '.r'))
+	knitr::purl(input = paste0(DIR, '/', filename, '.rmd'), output = paste0(DIR, '/', filename, '.r'))
+	source(paste0(DIR, '/', filename, '.r'))
+	file.remove(paste0(DIR, '/', filename, '.r'))
+	message('Finished - ', filename)
+})
+
+
+send.mail(
+	from = sender,
+	to = recipients,
+	subject = "Subject of the email",
+	body = "Body of the email",
+	smtp = list(
+		host.name = "smtp.gmail.com", port = 465, 
+		user.name = "YOURUSERNAME@gmail.com",            
+		passwd = "YOURPASSWORD",
+		ssl = TRUE
+		),
+	authenticate = TRUE,
+	send = TRUE
+	)
