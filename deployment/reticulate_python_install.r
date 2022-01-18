a50c0bad87e5c53ad5e2e99e2ee393030fc6a148
unknown <charles@cmefi.com>
2022-01-17 22:00:19 -0500

    Added mortage imports to FNMA scrape (v0.20)

--- deployment/reticulate_python_install.r
+++ deployment/reticulate_python_install.r
@@ -1,7 +1,15 @@
+# On Linux, will need to install libsqlite3
 library(reticulate)
-
 install_python(version = "3.8.7")
 use_python_version('3.8.7')
 virtualenv_create('econforecasting')
 virtualenv_install('econforecasting', 'camelot-py')
 virtualenv_install('econforecasting', 'opencv-python')
+
+
+# Reset
+library(reticulate)
+use_python_version('3.8.7')
+virtualenv_remove('econforecasting')
+# Look for dir in py_config
+system('rm -rf /home/charles/.pyenv')
