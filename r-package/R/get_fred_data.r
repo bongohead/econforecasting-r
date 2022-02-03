b2f66b88eef48e181245878f8e7acf5c7926dccd
Charles Ye <charles@cmefi.com>
2022-02-03 15:55:15 -0500

    Fixed bug in get_fred_data (v0.20)

--- r-package/R/get_fred_data.r
+++ r-package/R/get_fred_data.r
@@ -15,24 +15,23 @@ get_fred_data = function(series_id, api_key, .freq = NULL, .return_vintages = FA
 
 	today = as_date(with_tz(now(), tz = 'America/Chicago'))
 
-	url =
-		paste0(
-			'https://api.stlouisfed.org/fred/series/observations?',
-			'series_id=', series_id,
-			'&api_key=', api_key,
-			'&file_type=json',
-			'&realtime_start=', if (.return_vintages == TRUE & is.null(.vintage_date)) '2000-01-01' else if (.return_vintages == TRUE & !is.null(.vintage_date)) .vintage_date else today,
-			'&realtime_end=', if (.return_vintages == TRUE & !is.null(.vintage_date)) .vintage_date else today,
-			'&obs_start=', '2000-01-01',
-			'&obs_end=', today,
-			if(!is.null(.freq)) paste0('&frequency=', .freq) else '',
-			'&aggregation_method=avg'
+	url = paste0(
+		'https://api.stlouisfed.org/fred/series/observations?',
+		'series_id=', series_id,
+		'&api_key=', api_key,
+		'&file_type=json',
+		'&realtime_start=', if (.return_vintages == TRUE & is.null(.vintage_date)) '2000-01-01' else if (.return_vintages == TRUE & !is.null(.vintage_date)) .vintage_date else today,
+		'&realtime_end=', if (.return_vintages == TRUE & !is.null(.vintage_date)) .vintage_date else today,
+		'&observation_start=', '2000-01-01',
+		'&observation_end=', today,
+		if(!is.null(.freq)) paste0('&frequency=', .freq) else '',
+		'&aggregation_method=avg'
 		)
 
 	if (.verbose == TRUE) message(url)
 
 	url %>%
-		httr::RETRY('GET', url = ., times = 10) %>%
+		httr::RETRY('GET', url = ., times = 10, pause_min = 20) %>%
 		httr::content(., as = 'parsed') %>%
 		.$observations %>%
 		purrr::map_dfr(., function(x) as_tibble(x)) %>%
