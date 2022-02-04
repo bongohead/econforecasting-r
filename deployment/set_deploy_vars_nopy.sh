5a7e200fa6086e9cc25cb751bcc09f068260bfc8
Charles Ye <charles@cmefi.com>
2022-02-04 11:09:03 -0500

    Improved deployment setup for Python install (v0.20)

--- deployment/set_deploy_vars_nopy.sh
+++ deployment/set_deploy_vars_nopy.sh
@@ -1,9 +1,15 @@
-# --- On Windows, run this in Git Bash ---
-# Deployment
+#' Run this file to set Environmental variables
+#' 
+#' --- On Windows, run this in Git Bash ---
+
 HOSTNAME=`hostname`
+
+# Environmental variables to store
 EF_DIR=$([ "$HOSTNAME" = "econforecasting.com" ] && echo "/home/charles/projects/econforecasting" || echo "D:/Onedrive/__Projects/econforecasting")
+# EF_PY=$([ "$HOSTNAME" = "econforecasting.com" ] && echo "/home/charles/.pyenv/versions/3.8.7/bin/python3.8" || echo "C:/Users/Charles/AppData/Local/r-reticulate/r-reticulate/pyenv/pyenv-win/versions/3.8.7/python.exe")
 
 echo ${EF_DIR}
+#echo ${EF_PY}
 
 # Push environmental variables to R home directory/.Renviron
 HOME_DIR=$([ "$HOSTNAME" = "econforecasting.com" ] && echo "/home/charles" || echo "C:/Users/Charles/Documents")
@@ -13,3 +19,5 @@ rm -rf ${HOME_DIR}/.Renviron
 cat <<EOF >${HOME_DIR}/.Renviron
 EF_DIR = ${EF_DIR}
 EOF
+
+echo "----- Complete -----"
