3c4bbe67547c4576830bbaaa0c18508944f48024
Charles Ye <charles@cmefi.com>
2022-02-15 15:02:12 -0500

    Added centralized logging table & renamed rates models (v0.20)

---
+++ modules/create-job-logs.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS public.job_logs;
+CREATE TABLE public.job_logs (
+	logname varchar(50) CONSTRAINT job_logs_logname_uk UNIQUE NOT NULL,
+	module varchar(50) NOT NULL,
+	log_date date NOT NULL,
+	log_group varchar(50) NOT NULL,
+	log_info json NOT NULL,
+	log_dttm timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT job_logs_pk PRIMARY KEY (logname, module, log_date, log_group)
+	);
---
+++ modules/interest-rate-model/interest-rate-model-create-sql-tables.r
@@ -0,0 +1,146 @@
+# Initialize ----------------------------------------------------------
+## Set Constants ----------------------------------------------------------
+JOB_NAME = 'interest-rate-model-create-sql-tables'
+EF_DIR = Sys.getenv('EF_DIR')
+
+## Log Job ----------------------------------------------------------
+if (interactive() == FALSE) {
+	sink_path = file.path(EF_DIR, 'logs', paste0(JOB_NAME, '.log'))
+	sink_conn = file(sink_path, open = 'at')
+	system(paste0('echo "$(tail -50 ', sink_path, ')" > ', sink_path,''))
+	lapply(c('output', 'message'), function(x) sink(sink_conn, append = T, type = x))
+	message(paste0('\n\n----------- START ', format(Sys.time(), '%m/%d/%Y %I:%M %p ----------\n')))
+}
+
+## Load Libs ----------------------------------------------------------
+library(tidyverse)
+library(httr)
+library(DBI)
+library(RPostgres)
+library(econforecasting)
+library(lubridate)
+
+## Load Connection Info ----------------------------------------------------------
+source(file.path(EF_DIR, 'model-inputs', 'constants.r'))
+db = dbConnect(
+	RPostgres::Postgres(),
+	dbname = CONST$DB_DATABASE,
+	host = CONST$DB_SERVER,
+	port = 5432,
+	user = CONST$DB_USERNAME,
+	password = CONST$DB_PASSWORD
+	)
+
+# Set Table Contents ---------------------------------------------------------
+
+## Variables Table ---------------------------------------------------------
+interest_rate_model_variables = read_csv(
+	file.path(EF_DIR, 'modules', 'interest-rate-model', 'interest-rate-model-variables.csv'),
+	col_types = 'ccclccc'
+	)
+
+
+# SQL ---------------------------------------------------------
+
+## Variables ---------------------------------------------------------
+local({
+	
+	dbExecute(db, 'DROP TABLE IF EXISTS interest_rate_model_variables CASCADE')
+	
+	dbExecute(
+		db,
+		'CREATE TABLE interest_rate_model_variables (
+			varname VARCHAR(50) CONSTRAINT interest_rate_model_variables_pk PRIMARY KEY,
+			fullname VARCHAR(255) CONSTRAINT interest_rate_model_variables_fullname_uk UNIQUE NOT NULL,
+			units VARCHAR(50) NOT NULL,
+			sa BOOLEAN NOT NULL,
+			hist_source VARCHAR(50) NOT NULL,
+			hist_source_key VARCHAR(50),
+			hist_source_freq CHAR(1),
+			hist_source_transform VARCHAR(50),
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+			)'
+		)
+	
+	interest_rate_model_variables %>%
+		transmute(
+			.,
+			varname, fullname, units, sa,
+			hist_source, hist_source_key, hist_source_freq, hist_source_transform
+			) %>%
+		create_insert_query(
+			.,
+			'interest_rate_model_variables',
+			'ON CONFLICT ON CONSTRAINT interest_rate_model_variables_pk DO UPDATE
+		    SET
+		    fullname=EXCLUDED.fullname,
+		    units=EXCLUDED.units,
+		    sa=EXCLUDED.sa,
+		    hist_source=EXCLUDED.hist_source,
+		    hist_source_key=EXCLUDED.hist_source_key,
+		    hist_source_freq=EXCLUDED.hist_source_freq,
+		    hist_source_transform=EXCLUDED.hist_source_transform'
+			) %>%
+		dbExecute(db, .)
+})
+
+## Hist Values ---------------------------------------------------------
+local({
+	
+	dbExecute(db, 'DROP TABLE IF EXISTS interest_rate_model_hist_values CASCADE')
+	
+	dbExecute(
+		db,
+		'CREATE TABLE interest_rate_model_hist_values (
+			sourcename VARCHAR(50) NOT NULL,
+			vdate DATE NOT NULL,
+			freq CHAR(1) NOT NULL,
+			varname VARCHAR(50) NOT NULL,
+			date DATE NOT NULL,
+			value NUMERIC(20, 4) NOT NULL,
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			PRIMARY KEY (sourcename, vdate, freq, varname, date),
+			CONSTRAINT interest_rate_model_hist_values_varname_fk FOREIGN KEY (varname)
+				REFERENCES interest_rate_model_variables (varname)
+				ON DELETE CASCADE ON UPDATE CASCADE
+			)'
+		)
+
+	dbExecute(
+		db,
+		'SELECT create_hypertable(
+			relation => \'interest_rate_model_hist_values\',
+			time_column_name => \'vdate\'
+			);
+		')
+})
+
+## Forecast Values ---------------------------------------------------------
+local({
+	
+	dbExecute(db, 'DROP TABLE IF EXISTS interest_rate_model_forecast_values CASCADE')
+	
+	dbExecute(
+		db,
+		'CREATE TABLE interest_rate_model_forecast_values (
+			vdate DATE NOT NULL,
+			freq CHAR(1) NOT NULL,
+			varname VARCHAR(50) NOT NULL,
+			date DATE NOT NULL,
+			value NUMERIC(20, 4) NOT NULL,
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+			PRIMARY KEY (vdate, freq, varname, date),
+			CONSTRAINT interest_rate_model_forecast_values_varnames_fk FOREIGN KEY (varname)
+				REFERENCES interest_rate_model_variables (varname)
+				ON DELETE CASCADE ON UPDATE CASCADE
+			)'
+	)
+	
+	dbExecute(
+		db,
+		'SELECT create_hypertable(
+			relation => \'interest_rate_model_forecast_values\',
+			time_column_name => \'vdate\'
+			);
+		')
+})
