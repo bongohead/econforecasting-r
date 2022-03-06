68cbf09fb48e06d5fd2dd67dbeb0baf4f9f1adc6
Charles Ye <charles@cmefi.com>
2022-03-06 15:00:28 -0500

    Misc fixes (v1.01)

--- modules/sentiment-analysis-run.r
+++ modules/sentiment-analysis-run.r
@@ -61,11 +61,12 @@ token =
 	.$access_token
 
 ## Pull Data --------------------------------------------------------
-pulled_data = accumulate(1:200, function(accum, i) {
+pulled_data = accumulate(1:100, function(accum, i) {
 	
 	message('***** Pull ', i, ' of 100')
 	query = list(
 		t = 'month',
+		# g = 'GLOBAL',
 		limit = 100,
 		show = 'all',
 		after = {if (i == 1) NULL else tail(accum, 1)$after}
@@ -74,190 +75,232 @@ pulled_data = accumulate(1:200, function(accum, i) {
 		paste0(names(.), '=', .) %>%
 		paste0(collapse = '&')
 	
-	# message(query)
+	message(query)
 	
 	http_result =
 		GET(
-			paste0('https://oauth.reddit.com/top?', query),
+			paste0('https://oauth.reddit.com/r/all/top?', query),
 			add_headers(c(
 				'User-Agent' = 'windows:SentimentAnalysis:v0.0.1 (by /u/dongobread)',
 				'Authorization' = paste0('bearer ', token)
 				))
 			)
 	
-	message('Calls: ', headers(http_result)$`x-ratelimit-remaining`, ' | ', headers(http_result)$`x-ratelimit-reset`)
-	result = content(http_result, 'parsed')
+	calls_remaining = as.integer(headers(http_result)$`x-ratelimit-remaining`)
+	reset_seconds = as.integer(headers(http_result)$`x-ratelimit-reset`)
+	print(calls_remaining)
+	if (calls_remaining == 0) Sys.sleep(reset_seconds)
+	message(str_glue('Calls Remaining: {calls_remaining} | Seconds to Reset: {reset_seconds}s'))
 
-	parsed = map_dfr(result$data$children, function(y) 
-		y[[2]] %>% keep(., ~ !is.null(.) && !is.list(.)) %>% as_tibble(.)
+	result = content(http_result, 'parsed')
+	if (is.null(result$data$after)) stop('Missing after')
+	
+	parsed =
+		lapply(result$data$children, function(y) 
+			y[[2]] %>% keep(., ~ !is.null(.) && !is.list(.)) %>% as_tibble(.)
 		) %>%
+		rbindlist(., fill = T) %>%
 		select(
 			.,
+			name, # Unique identifier
 			subreddit, selftext, title, upvote_ratio, ups,
 			score, post_hint, is_self, created, domain, url_overridden_by_dest
 			) %>%
-		bind_cols(i = i, after = result$data$after, .)
-	
-	return(bind_rows(accum, parsed))
-	
-	}, .init = tibble()) %>%
-	bind_rows(.)
+		as.data.table(.) %>%
+		.[, c('i', 'after') := list(i, result$data$after)]
 
+	return(rbindlist(list(accum, parsed)))
+	}, .init = data.table()) %>%
+	rbindlist(.)
 
+# Verify no duplicated after posts
+pulled_data %>%
+	group_by(., i) %>%
+	summarize(., uniq_after = paste0(unique(after), collapse = ','))
+
+## Analysis --------------------------------------------------------
+dict =
+	get_sentiments('nrc') %>%
+	as.data.table(.) %>%
+	.[, sentiment := fcase(
+		sentiment %chin% c('trust', 'positive', 'joy', 'anticipation', 'surprise'), 1,
+		sentiment %chin% c('disgust', 'sadness', 'fear', 'negative', 'anger'), 0,
+		default = 0
+		)]
+
+pulled_data %>%
+	as.data.table(.) %>%
+	.[, created_dttm := as_datetime(created)] %>%
+	.[, created_date := as_date(created_dttm)] %>%
+	.[, c('created_dttm', 'created_date', 'subreddit', 'domain', 'title')] %>%
+	tidytext::unnest_tokens(., word, title) %>%
+	.[!word %chin% stop_words$word]  %>%
+	merge(., dict, by = 'word', all = F, allow.cartesian = T) %>%
+	.[, list(sentiment_score = sum(sentiment)/.N), by =
+# 		dplyr::left_join(
+# 			.,
+# 			dplyr::group_by(., date) %>%
+# 				dplyr::summarize(., dateTotal = n(), .groups = 'drop'),
+# 			by = 'date'
+# 		) %>%
+# 		dplyr::group_by(., sentiment, date) %>%
+# 		dplyr::summarize(., count = n(), dateTotal = unique(dateTotal), .groups = 'drop') %>%
+# 		dplyr::mutate(., percent = count/dateTotal) %>%
+# 		ggplot(.) + geom_line(aes(x = date, y = percent, color = sentiment, group = sentiment))
 
 
 # Pull Data
 
 ## Web Scrape
-local({
-	
-	sentMeta = DBI::dbGetQuery(db, 'SELECT * FROM sent_meta') %>% as_tibble(.)
-	
-	rawDf =
-		# Iterate through pages
-		# Go up to 3000
-		purrr::reduce(1:3000, function(accum, page) {
-			#if (page %% 20 == 1)
-			message('Downloading data for page ', page)
-			pageContent =
-				httr::GET(paste0('https://www.reuters.com/news/archive/businessnews?view=page&page=', page, '&pageSize=10')) %>%
-				httr::content(.) %>%
-				rvest::html_node(., 'div.column1')
-			
-			res =
-				tibble(
-					page = page,
-					article1 = html_text(html_nodes(pageContent, 'h3.story-title'), trim = TRUE),
-					article2 = html_text(html_nodes(pageContent, 'div.story-content > p'), trim = TRUE),
-					date = html_text(html_nodes(pageContent, 'span.timestamp'), trim = TRUE)
-				) %>%
-				mutate(
-					.,
-					date = ifelse(str_detect(date, coll('EDT')) == TRUE, format(Sys.Date(), '%b %d %Y'), date),
-					date = as.Date(parse_date_time2(date, '%b %d %Y'))
-				) %>%
-				bind_rows(accum, .)
-			
-			if (any(as.Date(res$date) %in% sentMeta$date)) return(done(res))
-			return(res)
-		}, .init = tibble()) %>%
-		dplyr::filter(., date < as.Date(Sys.Date()) & !date %in% sentMeta$date)
-	
-	
-	rawDf <<- rawDf
-})
+# local({
+# 	
+# 	sentMeta = DBI::dbGetQuery(db, 'SELECT * FROM sent_meta') %>% as_tibble(.)
+# 	
+# 	rawDf =
+# 		# Iterate through pages
+# 		# Go up to 3000
+# 		purrr::reduce(1:3000, function(accum, page) {
+# 			#if (page %% 20 == 1)
+# 			message('Downloading data for page ', page)
+# 			pageContent =
+# 				httr::GET(paste0('https://www.reuters.com/news/archive/businessnews?view=page&page=', page, '&pageSize=10')) %>%
+# 				httr::content(.) %>%
+# 				rvest::html_node(., 'div.column1')
+# 			
+# 			res =
+# 				tibble(
+# 					page = page,
+# 					article1 = html_text(html_nodes(pageContent, 'h3.story-title'), trim = TRUE),
+# 					article2 = html_text(html_nodes(pageContent, 'div.story-content > p'), trim = TRUE),
+# 					date = html_text(html_nodes(pageContent, 'span.timestamp'), trim = TRUE)
+# 				) %>%
+# 				mutate(
+# 					.,
+# 					date = ifelse(str_detect(date, coll('EDT')) == TRUE, format(Sys.Date(), '%b %d %Y'), date),
+# 					date = as.Date(parse_date_time2(date, '%b %d %Y'))
+# 				) %>%
+# 				bind_rows(accum, .)
+# 			
+# 			if (any(as.Date(res$date) %in% sentMeta$date)) return(done(res))
+# 			return(res)
+# 		}, .init = tibble()) %>%
+# 		dplyr::filter(., date < as.Date(Sys.Date()) & !date %in% sentMeta$date)
+# 	
+# 	
+# 	rawDf <<- rawDf
+# })
 
 
 ## Data to SQL
-local({
-	
-	
-	rowsAdded =
-		rawDf %>%
-		transmute(., date, page, article1, article2) %>%
-		mutate(., split = ceiling((1:nrow(.))/100)) %>% # Sent 100 max data points at a time
-		group_by(., split) %>%
-		group_split(., .keep = FALSE) %>%
-		imap(., function(x, i) {
-			message('Sending data to SQL ... ', i)
-			econforecasting::createInsertQuery(x, 'sent_data') %>%
-				DBI::dbExecute(db, .)
-		}) %>% unlist(.) %>% {if (any(is.null(.))) stop('SQL Error!') else sum(.)}
-	print(rowsAdded)
-})
+# local({
+# 	
+# 	
+# 	rowsAdded =
+# 		rawDf %>%
+# 		transmute(., date, page, article1, article2) %>%
+# 		mutate(., split = ceiling((1:nrow(.))/100)) %>% # Sent 100 max data points at a time
+# 		group_by(., split) %>%
+# 		group_split(., .keep = FALSE) %>%
+# 		imap(., function(x, i) {
+# 			message('Sending data to SQL ... ', i)
+# 			econforecasting::createInsertQuery(x, 'sent_data') %>%
+# 				DBI::dbExecute(db, .)
+# 		}) %>% unlist(.) %>% {if (any(is.null(.))) stop('SQL Error!') else sum(.)}
+# 	print(rowsAdded)
+# })
 
 
 
 
 ## Text Analysis
-local({
-	
-	
-	emotionsPlot =
-		rawDf %>%
-		dplyr::mutate(
-			.,
-			article = paste0(article1, ' ', article2),
-			date = ifelse(str_detect(date, coll('EDT')) == TRUE, format(Sys.Date(), '%b %d %Y'), date),
-			date = as.Date(parse_date_time2(date, '%b %d %Y'))
-		) %>%
-		tidytext::unnest_tokens(., word, article) %>%
-		dplyr::anti_join(., stop_words, by = 'word') %>%
-		dplyr::inner_join(
-			.,
-			get_sentiments('nrc') %>%
-				dplyr::mutate(
-					sentiment = ifelse(sentiment == 'disgust', 'negative', sentiment),
-					sentiment = ifelse(sentiment == 'sadness', 'negative', sentiment),
-					sentiment = ifelse(sentiment == 'disgust', 'negative', sentiment),
-					sentiment = ifelse(sentiment == 'anticipation', 'neutral', sentiment),
-				),
-			by = 'word'
-		) %>%
-		dplyr::left_join(
-			.,
-			dplyr::group_by(., date) %>%
-				dplyr::summarize(., dateTotal = n(), .groups = 'drop'),
-			by = 'date'
-		) %>%
-		dplyr::group_by(., sentiment, date) %>%
-		dplyr::summarize(., count = n(), dateTotal = unique(dateTotal), .groups = 'drop') %>%
-		dplyr::mutate(., percent = count/dateTotal) %>%
-		ggplot(.) + geom_line(aes(x = date, y = percent, color = sentiment, group = sentiment))
-	
-	rawDf %>%
-		dplyr::mutate(
-			.,
-			article = paste0(article1, ' ', article2),
-			date = ifelse(str_detect(date, coll('EDT')) == TRUE, format(Sys.Date(), '%b %d %Y'), date),
-			date = as.Date(parse_date_time2(date, '%b %d %Y'))
-		) %>%
-		tidytext::unnest_tokens(., word, article) %>%
-		dplyr::anti_join(., stop_words, by = 'word') %>%
-		dplyr::inner_join(
-			.,
-			get_sentiments('nrc') %>%
-				dplyr::mutate(
-					sentiment = case_when(
-						sentiment == 'disgust' ~ -1,
-						sentiment == 'sadness' ~ -1,
-						sentiment == 'anticipation' ~ 0,
-						sentiment == 'anger' ~ -2,
-						sentiment == 'fear' ~ -2,
-						sentiment == 'joy' ~ 1,
-						sentiment == 'surprise' ~ 0,
-						sentiment == 'trust' ~ 1,
-						sentiment == 'positive' ~ 1,
-						sentiment == 'negative' ~ -1,
-						sentiment == 'neutral' ~ 0,
-						TRUE ~ 0
-					)
-				),
-			by = 'word') %>%
-		dplyr::left_join(
-			.,
-			dplyr::group_by(., date) %>%
-				dplyr::summarize(., dateTotal = n(), .groups = 'drop'),
-			by = 'date'
-		) %>%
-		dplyr::group_by(., sentiment, date) %>%
-		dplyr::summarize(., count = n(), dateTotal = unique(dateTotal), .groups = 'drop') %>%
-		dplyr::mutate(., percent = count/dateTotal) %>%
-		dplyr::mutate(., weightedSentiment = sentiment * percent) %>%
-		dplyr::group_by(date) %>%
-		dplyr::summarize(., sentimentIndex = sum(weightedSentiment), .groups = 'drop') %>%
-		dplyr::mutate(., sentimentIndexLagged = roll::roll_mean(sentimentIndex, 7)) %>%
-		ggplot(.) +
-		geom_line(aes(x = date, y = sentimentIndexLagged))
-	
-	
-	# Benfords Law
-	rawDf %>%
-		dplyr::mutate(
-			.,
-			article = paste0(article1, ' ', article2),
-			date = ifelse(str_detect(date, coll('EDT')) == TRUE, format(Sys.Date(), '%b %d %Y'), date),
-			date = as.Date(parse_date_time2(date, '%b %d %Y'))
-		) %>%
-		tidytext::unnest_tokens(., word, article) %>% dplyr::filter(., str_detect(word, '^[0-9]*$')) %>% dplyr::mutate(., start = str_sub(word, 1, 1)) %>% dplyr::group_by(., start) %>% dplyr::summarize(., n = n()) %>% ggplot(.) + geom_col(aes(x = start, y = n))
-})
+# local({
+# 	
+# 	
+# 	emotionsPlot =
+# 		rawDf %>%
+# 		dplyr::mutate(
+# 			.,
+# 			article = paste0(article1, ' ', article2),
+# 			date = ifelse(str_detect(date, coll('EDT')) == TRUE, format(Sys.Date(), '%b %d %Y'), date),
+# 			date = as.Date(parse_date_time2(date, '%b %d %Y'))
+# 		) %>%
+# 		tidytext::unnest_tokens(., word, article) %>%
+# 		dplyr::anti_join(., stop_words, by = 'word') %>%
+# 		dplyr::inner_join(
+# 			.,
+# 			get_sentiments('nrc') %>%
+# 				dplyr::mutate(
+# 					sentiment = ifelse(sentiment == 'disgust', 'negative', sentiment),
+# 					sentiment = ifelse(sentiment == 'sadness', 'negative', sentiment),
+# 					sentiment = ifelse(sentiment == 'disgust', 'negative', sentiment),
+# 					sentiment = ifelse(sentiment == 'anticipation', 'neutral', sentiment),
+# 				),
+# 			by = 'word'
+# 		) %>%
+# 		dplyr::left_join(
+# 			.,
+# 			dplyr::group_by(., date) %>%
+# 				dplyr::summarize(., dateTotal = n(), .groups = 'drop'),
+# 			by = 'date'
+# 		) %>%
+# 		dplyr::group_by(., sentiment, date) %>%
+# 		dplyr::summarize(., count = n(), dateTotal = unique(dateTotal), .groups = 'drop') %>%
+# 		dplyr::mutate(., percent = count/dateTotal) %>%
+# 		ggplot(.) + geom_line(aes(x = date, y = percent, color = sentiment, group = sentiment))
+# 	
+# 	rawDf %>%
+# 		dplyr::mutate(
+# 			.,
+# 			article = paste0(article1, ' ', article2),
+# 			date = ifelse(str_detect(date, coll('EDT')) == TRUE, format(Sys.Date(), '%b %d %Y'), date),
+# 			date = as.Date(parse_date_time2(date, '%b %d %Y'))
+# 		) %>%
+# 		tidytext::unnest_tokens(., word, article) %>%
+# 		dplyr::anti_join(., stop_words, by = 'word') %>%
+# 		dplyr::inner_join(
+# 			.,
+# 			get_sentiments('nrc') %>%
+# 				dplyr::mutate(
+# 					sentiment = case_when(
+# 						sentiment == 'disgust' ~ -1,
+# 						sentiment == 'sadness' ~ -1,
+# 						sentiment == 'anticipation' ~ 0,
+# 						sentiment == 'anger' ~ -2,
+# 						sentiment == 'fear' ~ -2,
+# 						sentiment == 'joy' ~ 1,
+# 						sentiment == 'surprise' ~ 0,
+# 						sentiment == 'trust' ~ 1,
+# 						sentiment == 'positive' ~ 1,
+# 						sentiment == 'negative' ~ -1,
+# 						sentiment == 'neutral' ~ 0,
+# 						TRUE ~ 0
+# 					)
+# 				),
+# 			by = 'word') %>%
+# 		dplyr::left_join(
+# 			.,
+# 			dplyr::group_by(., date) %>%
+# 				dplyr::summarize(., dateTotal = n(), .groups = 'drop'),
+# 			by = 'date'
+# 		) %>%
+# 		dplyr::group_by(., sentiment, date) %>%
+# 		dplyr::summarize(., count = n(), dateTotal = unique(dateTotal), .groups = 'drop') %>%
+# 		dplyr::mutate(., percent = count/dateTotal) %>%
+# 		dplyr::mutate(., weightedSentiment = sentiment * percent) %>%
+# 		dplyr::group_by(date) %>%
+# 		dplyr::summarize(., sentimentIndex = sum(weightedSentiment), .groups = 'drop') %>%
+# 		dplyr::mutate(., sentimentIndexLagged = roll::roll_mean(sentimentIndex, 7)) %>%
+# 		ggplot(.) +
+# 		geom_line(aes(x = date, y = sentimentIndexLagged))
+# 	
+# 	
+# 	# Benfords Law
+# 	rawDf %>%
+# 		dplyr::mutate(
+# 			.,
+# 			article = paste0(article1, ' ', article2),
+# 			date = ifelse(str_detect(date, coll('EDT')) == TRUE, format(Sys.Date(), '%b %d %Y'), date),
+# 			date = as.Date(parse_date_time2(date, '%b %d %Y'))
+# 		) %>%
+# 		tidytext::unnest_tokens(., word, article) %>% dplyr::filter(., str_detect(word, '^[0-9]*$')) %>% dplyr::mutate(., start = str_sub(word, 1, 1)) %>% dplyr::group_by(., start) %>% dplyr::summarize(., n = n()) %>% ggplot(.) + geom_col(aes(x = start, y = n))
+# })
