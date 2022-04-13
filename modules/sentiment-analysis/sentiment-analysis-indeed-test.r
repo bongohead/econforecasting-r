35e4c9f9f8b33148c7a2e8b7682101dee709543d
Charles Ye <charles@cmefi.com>
2022-04-12 22:52:32 -0400

    Added Indeed test scraper (v1.01)

--- modules/sentiment-analysis/sentiment-analysis-indeed-test.r
+++ modules/sentiment-analysis/sentiment-analysis-indeed-test.r
@@ -8,8 +8,6 @@
 JOB_NAME = 'sentiment-analysis-indeed-scrape'
 EF_DIR = Sys.getenv('EF_DIR')
 RESET_SQL = FALSE
-BACKFILL_REDDIT = TRUE
-BACKFILL_REUTERS = TRUE
 
 ## Cron Log ----------------------------------------------------------
 if (interactive() == FALSE && rstudioapi::isAvailable(child_ok = T) == F) {
@@ -44,39 +42,163 @@ db = dbConnect(
 )
 
 # Scrape ----------------------------------------------------------
+
 ## Scrape ----------------------------------------------------------
 
-page_data =
-	GET('https://www.indeed.com/jobs?l=31904') %>%
-	content(.) %>%
-	html_node(., '#mosaic-zone-jobcards')
+page_html =
+	GET('https://www.indeed.com/jobs?l=31904&fromage=1&radius=25') %>%
+	content(.)
 
-page_info =
-	page_data %>%
-	html_nodes('a.tapItem') %>%
-	map_dfr(., function(x) tibble(
-		job_title = x %>% html_node(., 'h2.jobTitle > span') %>% html_text(.),
-		job_id = x %>% html_attr(., 'data-jk'),
-		company_name = x %>% html_node(., 'span.companyName') %>% html_text(.),
-		company_location = x %>% html_node(., 'div.companyLocation') %>% html_text(.),
-		job_snippet = x %>% html_node(., 'div.job-snippet') %>% html_text(.)
-		))
-
-page_data =
-	page_info %>%
-	purrr::transpose(.) %>%
-	.[1:1] %>%
-	lapply(., function(x) {
-		job_embed =
-			GET(str_glue('https://www.indeed.com/viewjob?viewtype=embedded&jk={x$job_id}')) %>%
-			content(.) %>%
-			html_node(., 'div.jobsearch-JobComponent')
-		
-		tibble(
-			job_title =
-				# https://stackoverflow.com/questions/56484967/scrape-first-class-node-but-not-child-using-rvest
-				job_embed %>%
-				html_node(., xpath = paste(selectr::css_to_xpath('h1.jobsearch-JobInfoHeader-title'), '/text()')) %>%
-				html_text(.)
+zip_scrape = purrr::reduce(1:1000, .init = tibble(), .f = function(accum, page_number) {
+	
+	message(str_glue('Scraping page {page_number}'))
+	
+	page_html = content(
+		GET(str_glue('https://www.indeed.com/jobs?l=31904&fromage=1&radius=25&start={(page_number - 1) * 10}')),
+		encoding = 'UTF-8'
 		)
-	})
+	
+	avail_pages = page_html %>% html_nodes('ul.pagination-list > li') %>% html_text(.) %>% keep(., ~ . != '')
+	this_page = page_html %>% html_node('ul.pagination-list > li > b') %>% html_text(.)
+	
+	message(str_glue('Available page {paste0(avail_pages, collapse = ", ")}'))
+	
+	page_data =
+		page_html %>%
+		html_node(., '#mosaic-zone-jobcards')
+	
+	page_info =
+		page_data %>%
+		html_nodes('a.tapItem') %>%
+		map_dfr(., function(x) tibble(
+			job_title = x %>% html_node(., 'h2.jobTitle > span') %>% html_text(.),
+			job_id = x %>% html_attr(., 'data-jk'),
+			company_name = x %>% html_node(., 'span.companyName') %>% html_text(.),
+			company_location = x %>% html_node(., 'div.companyLocation') %>% html_text(.),
+			job_snippet = x %>% html_node(., 'div.job-snippet') %>% html_text(.)
+			))
+	
+	page_data =
+		page_info %>%
+		purrr::transpose(.) %>%
+		imap_dfr(., function(x, i) {
+			# message(i)
+			job_embed =
+				GET(str_glue('https://www.indeed.com/viewjob?viewtype=embedded&jk={x$job_id}')) %>%
+				content(.) %>%
+				html_node(., 'div.jobsearch-JobComponent')
+			
+			job_header = tibble(
+				scrape_target = c('job_title', 'company_name', 'company_location'),
+				scrape_value = c(
+					# https://stackoverflow.com/questions/56484967/scrape-first-class-node-but-not-child-using-rvest
+					job_embed %>%
+						html_node(., xpath = paste(selectr::css_to_xpath('h1.jobsearch-JobInfoHeader-title'), '/text()')) %>%
+						html_text(.),
+					job_embed %>%
+						html_nodes(., 'div.jobsearch-InlineCompanyRating > div') %>%
+						.[2] %>%
+						html_text(.),
+					job_embed %>%
+						html_node(., 'div.jobsearch-InlineCompanyRating + div') %>%
+						html_text(.)
+					)
+				)
+			
+			job_details =
+				job_embed %>%
+				html_nodes(., 'div.jobsearch-JobDescriptionSection-sectionItem') %>%
+				map_dfr(., function(z)
+					tibble(
+						scrape_target =
+							html_node(z, 'div.jobsearch-JobDescriptionSection-sectionItemKey') %>%
+							html_text(.),
+						scrape_value =
+							html_nodes(z, '*:not(div.jobsearch-JobDescriptionSection-sectionItemKey)') %>%
+							html_text(.) %>%
+							paste0(., collapse = '\n')
+					)
+				) %>%
+				{
+					if (!is.null(.) && nrow(.) > 0) 
+						mutate(
+							.,
+							scrape_target = case_when(
+								str_detect(scrape_target, 'Job Type') ~ 'job_type',
+								str_detect(scrape_target, 'Salary') ~ 'salary',
+								TRUE ~ 'other_details'
+							)
+						)
+					else tibble()
+				}
+			
+	
+			job_quals =
+				job_embed %>%
+				html_nodes(., 'li.jobsearch-ReqAndQualSection-item') %>%
+				{
+					if (length(.) == 0) tibble()
+					else 
+						tibble(
+							scrape_target = 'job_quals',
+							scrape_value = 
+								html_text(.) %>% 
+								paste0(., collapse = '\n')
+						)
+				}
+			
+			job_description =
+				job_embed %>%
+				html_nodes(., '#jobDescriptionText') %>%
+				html_children(.) %>%
+				{
+					if (length(.) == 0) tibble()
+					else 
+						tibble(
+							scrape_target = 'job_description',
+							scrape_value = 
+								html_text(.) %>%
+								paste0(., collapse = '') %>%
+								str_trim(.)
+						)
+				}
+			
+			job_activity =
+				job_embed %>%
+				html_nodes(., '.jobsearch-HiringInsights-entry--bullet') %>%
+				html_text(.) %>%
+				tibble(scrape_value = .) %>%
+				{
+					if (!is.null(.) && nrow(.) > 0) 
+						mutate(
+							.,
+							scrape_target = case_when(
+								str_detect(scrape_value, 'Posted') ~ 'posted_time',
+								str_detect(scrape_value, 'Employer') ~ 'employer_id',
+								TRUE ~ 'other_activity'
+								),
+							scrape_value = case_when(
+								str_detect(scrape_value, 'Posted') ~ str_replace(scrape_value, 'Posted ', ''),
+								str_detect(scrape_value, 'Employer') ~ str_replace(scrape_value, 'Employer reviewed job ', ''),
+								TRUE ~ scrape_value
+							)
+						)
+					else tibble()
+				}
+	
+			res = 
+				bind_rows(
+					accum,
+					job_details,
+					job_header,
+					job_quals,
+					job_description,
+					job_activity
+				) %>%
+				mutate(., indeed_id = x$job_id, scrape_page_number = page_number)
+		})
+	
+	if (this_page == tail(avail_pages, 1)) return(done(page_data))
+	
+	return(page_data)
+})
