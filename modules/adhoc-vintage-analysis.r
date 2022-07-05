42e739139dc47d2de53b5039f837ebdeab74694d
Charles Ye <charles@cmefi.com>
2022-07-05 16:39:44 -0400

    Added WSS test for CME site (v1.03)

--- modules/adhoc-vintage-analysis.r
+++ modules/adhoc-vintage-analysis.r
@@ -25,7 +25,9 @@ db = dbConnect(
 
 
 # Get Data ----------------------------------------------------------
-as_tibble(dbGetQuery(db, str_glue(
+
+## Get Data ----------------------------------------------------------
+data_new = as_tibble(dbGetQuery(db, str_glue(
 	"SELECT
 		vdate, date, value
 	FROM forecast_values
@@ -33,8 +35,44 @@ as_tibble(dbGetQuery(db, str_glue(
 		AND freq = 'm'
 		AND varname = 'ffr'
 		AND form = 'd1'"
-	))) %>%
+	)))
+
+data_old = as_tibble(dbGetQuery(db, str_glue(
+	"SELECT
+		vdate, date, value
+	FROM rates_model_quandl
+	WHERE varname = 'ffr'"
+)))
+
+data_flat = 
+	bind_rows(data_new, data_old) %>%
+	filter(
+		.,
+		vdate >= as_date('2020-01-01'),
+		date <= as_date('2023-01-01')
+		)
+
+
+## Plot & Export ----------------------------------------------------------
+plot =
+	data_flat %>%
+	filter(., date == as_date('2022-12-01')) %>%
+	mutate(., date = as.character(date)) %>%
+	ggplot(.) +
+	geom_line(aes(x = vdate, y = value))
+
+print(plot)
+
+data_flat %>%
 	pivot_wider(., id_cols = 'vdate', names_from = 'date', values_from = 'value') %>%
 	View(.)
+
+## Model Deprecated ----------------------------------------------------------
+data_flat = as_tibble(dbGetQuery(db, str_glue(
+	"SELECT
+		varname, vdate, date, value
+	FROM rates_model_quandl
+	WHERE varname = 'ffr'"
+)))
 
 	
