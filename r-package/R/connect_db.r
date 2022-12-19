0319a760bc73e140eb1202069684becd2f9f310c
Charles Ye <charles@cmefi.com>
2022-12-19 13:00:40 -0500

    Added DB connectors (v2.00)

---
+++ r-package/R/connect_db.r
@@ -0,0 +1,51 @@
+#' Connect to main Postgres/TimescaleDB database
+#'
+#' @description
+#' Connect to Postgres/TimescaleDB database. You can either pass secrets directly through a list,
+#'  a YAML filepath, or seperate named arguments.
+#'
+#' @param secrets A list with database secrets key-value pairs
+#' @param secrets_path A filepath to a constants YAML file with database secrets key-value pairs
+#' @param ... Optional arguments with database secret key-value pairs
+#'
+#' @importFrom RPostgres Postgres
+#' @importFrom DBI dbConnect
+#' @export
+connect_db = function(secrets_list = NULL, secrets_path = NULL, ...) {
+
+	args = list(...)
+
+	if (any(c('DB_DATABASE', 'DB_SERVER', 'DB_USERNAME', 'DB_PASSWORD', 'DB_PORT') %in% names(args))) {
+		if (!all(c('DB_DATABASE', 'DB_SERVER', 'DB_USERNAME', 'DB_PASSWORD', 'DB_PORT') %in% names(args))) {
+			stop('Pass either secrets, secrets_path, or all of DB_DATABASE, DB_SERVER, DB_USERNAME, DB_PASSWORD, and DB_PORT.')
+		}
+		secrets = list(
+			DB_DATABASE = args$DB_DATABASE,
+			DB_SERVER = args$DB_SERVER,
+			DB_PORT = args$DB_PORT,
+			DB_USERNAME = args$DB_USERNAME,
+			DB_PASSWORD = args$DB_PASSWORD
+		)
+	} else if (!is.null(secrets_list)) {
+		if (!all(c('DB_DATABASE', 'DB_SERVER', 'DB_USERNAME', 'DB_PASSWORD', 'DB_PORT') %in% names(secrets_list))) {
+			stop('Secrets list must include DB_DATABASE, DB_SERVER, DB_USERNAME, DB_PASSWORD, and DB_PORT.')
+		}
+		secrets = secrets_list
+	} else if (!is.null(secrets_path)) {
+		secrets = get_secrets(secrets_path, c('DB_DATABASE', 'DB_SERVER', 'DB_USERNAME', 'DB_PASSWORD', 'DB_PORT'))
+	} else {
+		stop('Pass either secrets, secrets_path, or all of DB_DATABASE, DB_SERVER, DB_USERNAME, DB_PASSWORD, and DB_PORT.')
+	}
+
+
+	db = dbConnect(
+		Postgres(),
+		dbname = secrets$DB_DATABASE,
+		host = secrets$DB_SERVER,
+		port = secrets$DB_PORT,
+		user = secrets$DB_USERNAME,
+		password = secrets$DB_PASSWORD
+	)
+
+	return(db)
+}
---
+++ r-package/R/get_secrets.r
@@ -0,0 +1,47 @@
+#' Imports secrets and validates keys
+#'
+#' @description
+#' Imports secrets and validates keys are there.
+#'
+#' @param path_to_secrets_file The path to the secrets file.
+#' @param validate_keys A character vector of secret keys to include.
+#'
+#' @importFrom yaml read_yaml
+#' @export
+get_secrets = function(path_to_secrets_file, validate_keys = c()) {
+	if (!is.character(path_to_secrets_file)) stop('Parameter path_to_secrets_file must be a string')
+	secrets = tryCatch(
+		{yaml::read_yaml(path_to_secrets_file)},
+		error = function(e) stop('Cannot read secrets file:\n ', e)
+	)
+	if (length(validate_keys) > 0) {
+		missing_any = validate_keys[!validate_keys %in% names(secrets)]
+		if (length(missing_any) > 0) stop('Missing in constants file : ', paste0(missing_any, collapse = ', '))
+	}
+
+	return(secrets)
+}
+
+#' Get secret secrets
+#'
+#' @description
+#' Imports secrets and validates keys are there.
+#'
+#' @param key The key of the key to return
+#' @param path_to_secrets_file The path to the secrets file.
+#'
+#' @importFrom yaml read_yaml
+#' @export
+get_secret = function(key, path_to_secrets_file) {
+
+	if (!is.character(key)) stop('Parameter path_to_secrets_file must be a string')
+	if (!is.character(path_to_secrets_file)) stop('Parameter path_to_secrets_file must be a string')
+
+	secrets = tryCatch(
+		{yaml::read_yaml(path_to_secrets_file)},
+		error = function(e) stop('Cannot read secrets file:\n ', e)
+	)
+	if (!key %in% names(secrets)) stop('Key does not exist in secrets file!')
+
+	return(secrets[key])
+}
