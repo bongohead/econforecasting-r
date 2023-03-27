329849ce1c87ee3b8e2cd9e1f288996e92ac2f6e
Charles Ye <charles@cmefi.com>
2023-03-27 02:05:21 -0400

    Cleanup (v2.02)

--- deployment/python-setup.r
+++ deployment/python-setup.r
@@ -3,9 +3,8 @@
 #' Deployment:
 #' (1) Install and update R
 #' (2) Install package reticulate
-#' (3) Run set_deploy_vars_nopy.sh; update EF_DIR if necessary
-#' (4) Run python_setup.r
-
+#' (3) Run set-deploy-vars-nopy.sh; update EF_DIR if necessary
+#' (4) Run python-setup.r
 if (!dir.exists(file.path(Sys.getenv('EF_DIR'), '.virtualenvs'))) {
 	dir.create(file.path(Sys.getenv('EF_DIR'), '.virtualenvs'), mode = '0755')
 }
@@ -19,12 +18,10 @@ virtualenv_create(file.path(Sys.getenv('EF_DIR'), '.virtualenvs', 'econforecasti
 # Img2pdf for SVG parsing to PDF
 # Matplotlib for diagnosing issues with camelot-py
 # Note: if Happytransformer fails on Windows, run  D:\OneDrive\__Projects\econforecasting\.virtualenvs\econforecasting\Scripts\python.exe -m pip install --upgrade pip' command
-# Socket.io
 # Removed svglib 7/5/22 - needed?
 lapply(c('camelot-py', 'opencv-python', 'requests', 'matplotlib', 'happytransformer'), function(x)
 	virtualenv_install(file.path(Sys.getenv('EF_DIR'), '.virtualenvs', 'econforecasting'), x)
 )
-
 
 # Reset
 library(reticulate)
